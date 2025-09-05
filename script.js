f9dfcda22dcbb59a573e25dfcd26e6000c5b6343
gabrielmz719 <g20998986@gmail.com>
2025-09-05 16:44:49 -0300

    feat:creating board and GameController player objects

---
+++ script.js
@@ -0,0 +1,19 @@
+
+function GameBoard(){
+    this.board=[
+        ['', '', ''],
+        ['', '', ''],
+        ['', '', '']
+    ]
+};
+
+function Player(name,figure){
+    this.name = name,
+    this.figure = figure
+};
+
+function GameController(){
+
+}
+
+
